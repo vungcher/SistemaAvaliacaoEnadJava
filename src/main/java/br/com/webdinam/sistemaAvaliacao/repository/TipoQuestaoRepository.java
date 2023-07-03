95ffc976bd6232d66acdbb639672b23c79b19d16
Matheus H Campos de Oliveira <vungcher@gmail.com>
2023-07-03 12:59:04 -0300

    Update

--- src/main/java/br/com/webdinam/sistemaAvaliacao/repository/TipoQuestaoRepository.java
+++ src/main/java/br/com/webdinam/sistemaAvaliacao/repository/TipoQuestaoRepository.java
@@ -10,21 +10,15 @@ import java.util.List;
 public interface TipoQuestaoRepository extends JpaRepository<TipoQuestao, Integer> {
     TipoQuestao findByNome(String nome);
 
-    // Método para contar o número total de tipos de questão
     long count();
 
-    // Método para verificar se um tipo de questão existe pelo nome
     boolean existsByNome(String nome);
 
-    // Método para excluir um tipo de questão pelo nome
     void deleteByNome(String nome);
 
-    // Método para encontrar todos os tipos de questão com o nome ordenado
     List<TipoQuestao> findAllByOrderByNome();
-
-    // Método para encontrar todos os tipos de questão que tenham a descrição fornecida no nome
+    
     List<TipoQuestao> findAllByNomeContainingIgnoreCase(String descricao);
 
-    // Método para encontrar todos os tipos de questão que tenham o nome começando com a letra fornecida
     List<TipoQuestao> findAllByNomeStartingWithIgnoreCase(String letra);
 }
